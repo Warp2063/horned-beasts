702cf588d69618ecd8c97af32883897b27adf81d
warp2063 <warp2063@gmail.com>
2023-04-06 07:28:45 -0400

    updated destructuring on HornedBeast.js I believe

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,14 +1,23 @@
+import {useState} from "react";
+
 function HornedBeast({title, description, imageUrl}) {
 
     //const title = {props.title};
     //const ImageUrl = props => <div>{props.children}</div>
     //const Description = props => <Tab><Label>Foo</Label></Tab>
 
+    const [votes, setVotes] = useState(0);
+
+    function clickHandler() {
+        setVotes(votes + 1);
+    }
+
     return (
         <div className="horned-beast">
             <h2>{title}</h2>
-            <img src={imageUrl} alt={title} title={title} />
+            <img onClick={clickHandler} src={imageUrl} alt={title} title={title} />
             <p>{description}</p>
+            <p>Votes: {votes}</p>
         </div>
     );
 }
